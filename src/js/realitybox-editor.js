a9b2b2bc431a5f7cd81b33a51ee23146c4f33f07
jballmann <johannes.ballmann@rwth-aachen.de>
2021-03-16 15:17:49 +0100

    esdoc comments added

--- src/js/realitybox-editor.js
+++ src/js/realitybox-editor.js
@@ -1,5 +1,13 @@
 H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
 
+  /**
+   * Constructor function
+   * @extends H5P.DragNBar.FormManager
+   * @param {Object} parent - Parent object
+   * @param {Object} field - Field object
+   * @param {Object} params - Config object for RealityBox
+   * @param {function(field: Object, value: Object)} - Callback function to set widgets field value
+   */
   function RealityBoxEditor(parent, field, params, setValue) {
 
     H5P.DragNBar.FormManager.call(
@@ -31,8 +39,6 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
       });
     });
 
-    this.isFirstModel = (params === undefined || !parent.params.video.files);
-
     this.params = $.extend({
       markers: []
     }, params);
@@ -54,9 +60,15 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
 
   }
 
+  // extends H5P.DragNBar.FormManager
   RealityBoxEditor.prototype = Object.create(H5P.DragNBar.FormManager.prototype);
   RealityBoxEditor.prototype.constructor = RealityBoxEditor;
 
+  /**
+   * Invokes callback with H5P field object of given path
+   * @param {string} path - Path of field
+   * @param {function(field: Object)} callback - Callback function
+   */
   RealityBoxEditor.prototype.findField = function (path, callback) {
     this.parent.ready(() => {
       const field = H5PEditor.findField(path, this.parent);
@@ -67,12 +79,19 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
     });
   }
 
+  /**
+   * Set model
+   * @param {Object} file - H5P file object
+   */
   RealityBoxEditor.prototype.setModel = function (file) {
     console.log(file);
     this.model = file;
     this.deleteBabylonBox();
   }
 
+  /**
+   * Creates BabylonBox instance in widget
+   */
   RealityBoxEditor.prototype.setActive = function () {
     if (this.babylonBox !== undefined) {
       return;
@@ -83,9 +102,8 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
       return;
     }
 
-    console.log('--- Editor model ---')
-    console.log(H5P.getPath(this.model.path, H5PEditor.contentId))
-    const params = $.extend({}, { file: this.model }, this.params);
+    let modelUrl = H5P.getPath(this.model.path, H5PEditor.contentId));
+    let params = $.extend({}, { modelUrl }, this.params);
     this.babylonBox = H5P.newRunnable({
       library: 'H5P.BabylonBox 1.0',
       params
@@ -104,10 +122,19 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
     this.babylonBox.attach(this.$editor);
   }
 
+  /**
+   * Alert message
+   * @param {string} message - Message to show
+   */
   RealityBoxEditor.prototype.displayMessage = function (message) {
     alert(message);
   }
 
+  /**
+   * Creates form for edit marker content
+   * @param {Marker} marker - Associated marker
+   * @param {Object} parameters - Parameters of markers content
+   */
   RealityBoxEditor.prototype.createMarkerForm = function (marker, parameters) {
     const $semanticFields = $('<div class="h5p-dialog-inner-semantics" />');
     marker.$form = $semanticFields;
@@ -118,6 +145,7 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
 
     H5PEditor.processSemanticsChunk(markerFields, parameters, $semanticFields, this);
 
+    // hide selector for choosing library
     var pos = markerFields.map(function (field) {
       return field.type;
     }).indexOf('library');
@@ -126,15 +154,23 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
     }
   }
 
+  /**
+   * Processes marker
+   * @param {Marker} marker - Marker to process
+   * @param {Object} parameters - Parameters of markers content
+   */
   RealityBoxEditor.prototype.processMarker = function (marker, parameters) {
     this.createMarkerForm(marker, parameters);
     marker.children = this.children;
     this.children = undefined;
-    const markerFields = this.getMarkerFields(marker);
-
     this.newMarker(marker);
   }
 
+  /**
+   * Gets H5P content field of specific marker
+   * @param {Marker} marker
+   * @return {Object} - H5P field for markers content
+   */
   RealityBoxEditor.prototype.getMarkerFields = function (marker) {
     return marker.children.reduce(function (prev, child) {
       if (child.field && child.field.name) {
@@ -144,6 +180,10 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
     }, {});
   }
 
+  /**
+   * Opens form for a specific marker
+   * @param {Marker} marker
+   */
   RealityBoxEditor.prototype.openMarkerModal = function (marker) {
     const formremoveHandler = () => {
       if(!confirm('Would you like to remove this marker?')) {
@@ -173,16 +213,37 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
     marker.trigger('openEditDialog');
   }
 
+  /**
+   * Opens form for new created marker
+   * @param {Marker} marker - Newly created marker
+   */
   RealityBoxEditor.prototype.newMarker = function (marker) {
       this.openMarkerModal(marker);
   }
 
+  /**
+   * Removes marker
+   * @param {Marker} marker - Marker to remove
+   */
   RealityBoxEditor.prototype.removeMarker = function (marker) {
     this.babylonBox.removeMarker(marker);
     this.params.markers.splice(marker.arrayPosition, 1);
     H5PEditor.removeChildren(marker.children);
   }
 
+  /**
+   * Adds marker
+   * @param {Object} positionObj - Position where new marker should created
+   * @param {number} positionObj.x - x position
+   * @param {number} positionObj.y - y position
+   * @param {number} positionObj.z - y position
+   * @param {Object} normalRefOject - Normal reference of marker and model
+   * @param {number} normalRefOject.x - x position
+   * @param {number} normalRefOject.y - y position
+   * @param {number} normalRefOject.z - z position
+   * @param {Object} options - Config object for marker
+   * @return {Marker} - New created marker instance
+   */
   RealityBoxEditor.prototype.addMarker = function (positionObj, normalRefObj, options) {
     let params = {};
     if (options) {
@@ -215,6 +276,9 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
     return this.marker;
   }
 
+  /**
+   * Deletes current BabylonBox instance
+   */
   RealityBoxEditor.prototype.deleteBabylonBox = function () {
     if (this.babylonBox !== undefined) {
       this.babylonBox.stopRendering();
@@ -222,16 +286,28 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
     }
   }
 
+  /**
+   * Appends H5P editor item scaffold to widgets wrapper
+   * @param {jQuery} $wrapper - wrapper of widget
+   */
   RealityBoxEditor.prototype.appendTo = function ($wrapper) {
     this.$item = $(this.createHTML()).appendTo($wrapper);
     this.$editor = this.$item.children('.h5peditor-markers');
     this.$errors = this.$item.children('.h5p-errors');
   }
 
+  /**
+   * Gets H5P editor item scaffold
+   * @return {string} - HTML string
+   */
   RealityBoxEditor.prototype.createHTML = function () {
     return H5PEditor.createItem(this.field.widget, '<div class="h5peditor-markers" />');
   }
 
+  /**
+   * Checks validity of all entries
+   * @return {boolean} True if all entries are valid
+   */
   RealityBoxEditor.prototype.validate = function () {
     if (this.babylonBox) {
       for (marker of this.babylonBox.getMarkers()) {
@@ -244,10 +320,14 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
     return true;
   }
 
+  /**
+   * Removes widget
+   */
   RealityBoxEditor.prototype.remove = function () {
     this.deleteBabylonBox();
     this.$item.remove();
   }
+
 
   RealityBoxEditor.prototype.ready = function (ready) {
     if (this.passReadies) {
@@ -258,6 +338,13 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
     }
   }
 
+  /**
+   * Find field by name
+   * @private
+   * @param {string} name - Name for seeking
+   * @param {Object[]} fields - Fields to go over
+   * @return {Object} - Field with the specific name
+   */
   const findField = function (name, fields) {
     for (f of fields) {
       if (f.name === name) {
@@ -266,6 +353,11 @@ H5PEditor.widgets.realitybox = H5PEditor.RealityBox = (function ($) {
     }
   }
 
+  /**
+   * Hides fields from widget by name
+   * @param {Object[]} markerFields - All marker fields
+   * @param {string[]} fields - Array of field names that should hided
+   */
   const hideFields = function (markerFields, fields) {
     // Find and hide fields in list
     for (f of fields) {
